94f76c3f32acf0f555f0e9811eadd685e204908d
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-11-23 18:06:24 +0200

    Update product repository

--- src/repositories/productRepository.ts
+++ src/repositories/productRepository.ts
@@ -1,21 +1,26 @@
 import prisma from '../prisma';
 
 export function findAllProducts() {
-  return prisma.product.findMany({ include: { productPrice: true, images: true, ProductAssigned: true } });
+  return 
+  // return prisma.bike.findMany({ include: { productPrice: true, images: true, ProductAssigned: true } });
 }
 
 export function findProductById(id: number) {
-  return prisma.product.findUnique({ where: { id }, include: { productPrice: true, images: true, ProductAssigned: true } });
+  return
+  // return prisma.product.findUnique({ where: { id }, include: { productPrice: true, images: true, ProductAssigned: true } });
 }
 
 export function createProduct(data: any) {
-  return prisma.product.create({ data, include: { productPrice: true, images: true, ProductAssigned: true } });
+  return
+  // return prisma.product.create({ data, include: { productPrice: true, images: true, ProductAssigned: true } });
 }
 
 export function updateProduct(id: number, data: any) {
-  return prisma.product.update({ where: { id }, data, include: { productPrice: true, images: true, ProductAssigned: true } });
+  return
+  // return prisma.product.update({ where: { id }, data, include: { productPrice: true, images: true, ProductAssigned: true } });
 }
 
 export function deleteProduct(id: number) {
-  return prisma.product.delete({ where: { id } });
+  return
+  // return prisma.product.delete({ where: { id } });
 }
