6e7a803bc489f95b92f92a889b779dae821d939e
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-11-26 09:38:46 +0200

    Remove unwanted code

--- src/repositories/rentalRepository.ts
+++ src/repositories/rentalRepository.ts
@@ -5,15 +5,15 @@ export function findAllRentals() {
 }
 
 export function findRentalById(id: number) {
-  return prisma.rental.findUnique({ where: { id }, include: { product: true } });
+  return prisma.rental.findUnique({ where: { id } });
 }
 
 export function createRental(data: any) {
-  return prisma.rental.create({ data, include: { product: true } });
+  return prisma.rental.create({ data });
 }
 
 export function updateRental(id: number, data: any) {
-  return prisma.rental.update({ where: { id }, data, include: { product: true } });
+  return prisma.rental.update({ where: { id }, data });
 }
 
 export function deleteRental(id: number) {
