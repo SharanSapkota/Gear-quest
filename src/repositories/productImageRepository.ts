34c5bf515ad280dc5d6b7487bc1944ed7e7f698d
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-11-26 09:38:38 +0200

    Remove unwanted code

--- src/repositories/productImageRepository.ts
+++ src/repositories/productImageRepository.ts
@@ -1,13 +1,13 @@
 import prisma from '../prisma';
 
-export function findImagesByProduct(productId: number) {
-  return prisma.productImage.findMany({ where: { productId } });
+export function findImagesByBike(bikeId: number) {
+  return 
 }
 
-export function createProductImage(data: any) {
-  return prisma.productImage.create({ data });
+export function createBikeImage(data: any) {
+  return 
 }
 
 export function deleteProductImage(id: number) {
-  return prisma.productImage.delete({ where: { id } });
+  return 
 }
