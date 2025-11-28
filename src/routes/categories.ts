650eb65409398193cc1ec30ec1d666c655c1b81c
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-11-28 19:34:42 +0200

    Add category api

--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import * as catCtrl from '../controllers/categoryController';
+import { authenticate } from '../middleware/auth';
 
 const router = Router();
 
 router.get('/', catCtrl.list);
-router.post('/', catCtrl.create);
+router.post('/', authenticate, catCtrl.create);
 
 export default router;
