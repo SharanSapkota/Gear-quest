80d3491d8166dcbc6b888cd61f379ae70234bad1
SharanSapkota <46062039+SharanSapkota@users.noreply.github.com>
2025-12-05 09:16:29 +0000

    Send res success

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -4,10 +4,12 @@ import { sendSuccess } from '../utils/response';
 
 export async function list(req: Request, res: Response) {
   const items = await catRepo.findAllCategories();
+  
   return sendSuccess(res, items, 200);
 }
 
 export async function create(req: Request, res: Response) {
   const item = await catRepo.createCategory(req.body);
+
   return sendSuccess(res, item, 201);
 }
