bd685ef5d2f0b65e606d70433760a5aea51ff741
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-12-08 16:33:43 +0200

    Send success

--- src/controllers/userEmailController.ts
+++ src/controllers/userEmailController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
 import * as emailService from '../services/userEmailService';
+import { sendSuccess } from '../utils/response';
 
 export async function list(req: Request, res: Response) {
   const userId = Number(req.params.userId || (req as any).user?.id);
   const items = await emailService.listEmails(userId);
-  res.json(items);
+  
+  return sendSuccess(res, items, 200);
 }
 
 export async function create(req: Request, res: Response) {
