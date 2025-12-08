1ca246d4215887dcd6e6298cfb0f8e62c6e40520
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-12-08 16:33:48 +0200

    Send success

--- src/controllers/userSecurityController.ts
+++ src/controllers/userSecurityController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
 import * as secService from '../services/userSecurityService';
+import { sendSuccess } from '../utils/response';
 
 export async function get(req: Request, res: Response) {
   const userId = Number(req.params.userId || (req as any).user?.id);
   const item = await secService.getSecurity(userId);
   if (!item) return res.status(404).json({ error: 'not found' });
-  res.json(item);
+  
+  return sendSuccess(res, item, 200);
 }
 
 export async function upsert(req: Request, res: Response) {
