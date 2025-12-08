ff45fd8004ed965dcf075e9dcbd54c3cb2ff89bf
Sharan Sapkota <sapkotarambbo@gmail.com>
2025-12-08 16:33:29 +0200

    Send success

--- src/controllers/reviewController.ts
+++ src/controllers/reviewController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
 import * as revRepo from '../repositories/reviewRepository';
+import { sendSuccess } from '../utils/response';
 
 export async function listByProduct(req: Request, res: Response) {
   const userId = Number(req.params.userId);
   const items = await revRepo.findReviewsByUser(userId);
-  res.json(items);
+  return sendSuccess(res, items, 200);
 }
 
 export async function create(req: Request, res: Response) {
